0822bbf0abed034e35995fb9c0bf232dd40ca98b
Corvuvr <d.podgornyy@inbox.ru>
2023-04-12 04:40:02 +0300

    Updated start window size

--- MyTracer/src/WalnutApp.cpp
+++ MyTracer/src/WalnutApp.cpp
@@ -215,11 +215,9 @@ public:
 #endif // SPHERES
 
 
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));	
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 		ImGui::Begin("Viewport");
-			
-			
-			//ImGui::SetWindowSize("Viewport", ImVec2(320, 240));
+	
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
